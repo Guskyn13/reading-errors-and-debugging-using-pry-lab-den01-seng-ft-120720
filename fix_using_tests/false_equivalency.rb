5ea77645f1274a5a0beb83bbc8c0b25658673b08
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-29 15:09:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -6,9 +6,9 @@ require 'pry'
 
 def selection(num)
   num =1
-  if num = 1
+  if num == 1
      puts "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
     puts  "HAM HAM HAM IN MY TUMMY"
   end
 

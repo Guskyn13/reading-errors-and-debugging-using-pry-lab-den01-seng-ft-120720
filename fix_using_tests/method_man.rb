755049d4892f54e238d423e1022ce442acaedef0
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-29 15:14:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,7 +2,7 @@ require 'pry'
 def start_game(player1, player2)
   player1 = "Rocky"
   player2 = "Bullwinkle"
-  puts "Hello #{player1} & #{playar2}"
+   "Hello #{player1} & #{playar2}"
 end
 binding.pry
 def play_game(player2)
